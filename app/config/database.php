cf5b05f71b67db72fed37e205850ce8fd8d057e8
Роман Васильев <rsvasilyev@gmail.com>
2013-02-20 23:25:49 +0200

    Set database connection parameters.

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'laravel-tutorial',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
